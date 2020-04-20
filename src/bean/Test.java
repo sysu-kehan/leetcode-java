eb29ee441793711afe8e7456073f0dc81037bb2b
sysukehan <sysukehan@163.com>
2020-04-20 19:24:15 +0300

    优化了TreeNode把数组转换为树的实现。

--- src/bean/Test.java
+++ src/bean/Test.java
@@ -1,6 +1,6 @@
 package bean;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Test {
   public static void main(String[] args) {
--- src/bean/TreeNode.java
+++ src/bean/TreeNode.java
@@ -62,39 +62,17 @@ public class TreeNode {
     if (array.length <= 0) {
       return null;
     }
-    TreeNode[] nodes = new TreeNode[array.length];
+    TreeNode[] nodes = new TreeNode[array.length + 1];
     for (int i = 0; i < array.length; i++) {
-      nodes[i] = array[i] == null ? null : new TreeNode(array[i]);
+      nodes[i + 1] = array[i] == null ? null : new TreeNode(array[i]);
     }
-    //  计算树一共有几层
-    int totalLevel = 0;
-    for (int sum = 1; array.length > sum; totalLevel++, sum += Math.pow(2, totalLevel)) ;
-    int[] levels = new int[totalLevel + 1];
-    levels[0] = 1;
-    for (int i = 1; i <= totalLevel; i++) {
-      levels[i] = (int) Math.pow(2, i) + levels[i - 1];
-    }
-    //  构造根节点
-    TreeNode root = nodes[0];
-    int rootLeftChildIndex = 1;
-    int rootRightChildIndex = rootLeftChildIndex + 1;
-    root.left = rootLeftChildIndex < nodes.length ? nodes[rootLeftChildIndex] : null;
-    root.right = rootRightChildIndex < nodes.length ? nodes[rootRightChildIndex] : null;
-
-    int curLevel = 1;
-    for (int i = 1; i < nodes.length; i++) {
-      if (i + 1 > levels[curLevel]) {
-        curLevel++;
-      }
-      if (nodes[i] != null && curLevel < totalLevel) {
-        int curPos = i - levels[curLevel - 1];
-        int childPos = curPos * 2;
-        int leftChildIndex = levels[curLevel] + childPos;
-        int rightChildIndex = leftChildIndex + 1;
-        nodes[i].left = leftChildIndex < nodes.length ? nodes[leftChildIndex] : null;
-        nodes[i].right = rightChildIndex < nodes.length ? nodes[rightChildIndex] : null;
+    int length = nodes.length;
+    for (int i = 1; i * 2 < length; i++) {
+      if (nodes[i] != null) {
+        nodes[i].left = nodes[i * 2];
+        nodes[i].right = i * 2 + 1 < length ? nodes[i * 2 + 1] : null;
       }
     }
-    return root;
+    return nodes[1];
   }
 }
